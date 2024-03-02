cf6a5b20bf00d08b4d2bdbcaf606748867a2a72b
Jayanth Manikanta Karri <jayanth.karri2004@gmail.com>
2024-03-02 05:27:43 +0000

    Created a chatbot

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    fastAPIUrl: "https://8000-monikakusumanc-reactapp-zn729xfad4j.ws-us108.gitpod.io/get_gemini_completion"
+    fastAPIUrl: "https://8000-jayanthmanik-llmfastapi-78m8ekx9wb5.ws-us108.gitpod.io/get_gemini_completion"
   };
